fb17d7d47e47d40394d790e253b29c6762aa8e03
Luth3rrr <92605914+Luth3rrr@users.noreply.github.com>
2024-09-12 19:32:42 +0300

    Update hiring.js

--- static/assets/uv/js/hiring.js
+++ static/assets/uv/js/hiring.js
@@ -2,7 +2,7 @@ function hire(value) {
   let iframe = document.querySelector(".iframe.active");
 
   window.navigator.serviceWorker
-    .register("/assets/uv/sw.js", {
+    .register("https://55gms.com/assets/uv/sw.js", {
       scope: __uv$config.prefix,
     })
     .then(() => {
